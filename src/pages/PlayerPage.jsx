8513aace28da545ce5715d08660684a677adb519
varditomer <tomer.vardi@gmail.com>
2025-04-04 19:09:02 +0300

    initial PlayerPage implementation

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import MusicNote from "../assets/images/music_note.png";
+
 
 export const PlayerPage = () => {
-  return <div>PlayerPage</div>;
+  return (
+    <div className="player-page">
+      <img src={MusicNote} alt="" className="music-note-img" />
+      <h2>Waiting for next song...</h2>
+    </div>
+  );
 };
